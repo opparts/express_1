e17bbdb81991d062edb30597b5ce9b47d2a42e15
Alex Liu <alex.gang.liu@gmail.com>
2017-03-10 09:01:47 +0800

    手动创建Express项目

--- app.js
+++ app.js
@@ -11,25 +11,35 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 
-
+// 为express构建一个实例对象
 var app = express();
 
 //app.set('port',process.evn.PORT || 3000);
-app.set('port',3000);
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine','jade');
+// 服务器到底应该监听那个端口号，针对当前的这个服务
+app.set('port', 3000);
+// 视图模板的绝对路径
+app.set('views', path.join(__dirname, 'views'));
 
 
-app.all('*',function(req,res){
-    res.render('index',{
-        msg:'Welcome'
+// 模板文件的扩展，例如jade，html。这里的view engine不能写错
+app.set('view engine', 'jade');
 
-    });
+
+app.all('*', function (req, res) {
+    res.render('index', {msg: 'Welcome'});
 
 });
 
-http.createServer(app).listen(app.get('port'),function () {
-    console.log('Express.js server listening on port:' + app.get('port'));
-});
+http
+    .createServer(app)
+    .listen(
+        app.get('port'),
+        '127.0.0.1',
+        function () {
+            console.log(
+                'Express.js server listening on port: 127.0.0.1:' + app.get('port')
+            );
+        }
+    );
 
 
